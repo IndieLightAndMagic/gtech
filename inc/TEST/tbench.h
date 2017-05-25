f75255a7f491d87e1f845b65d6da568c8b3c4ba4
Julian Andres Guarin Reyes <julian.guarin@fitech.com.co>
2017-05-24 20:12:13 -0500

    Test bench included in a header@

---
+++ inc/TEST/tbench.h
@@ -0,0 +1,19 @@
+#ifndef __TBENCH_H__
+#define __TBENCH_H__
+
+#define UINT_TEST_OK 1
+#define UINT_TEST_FAIL 0 
+#define BOOL_TEST_OK true
+#define BOOL_TEST_FAIL false 
+
+
+#define TEST_INT(prb,xpr) prb = prb &(xpr==1?true:false) \
+	std::cout << __FILE__ << ":" << __LINE__ << ":[TST]" << (prb ? "OK":"FAILED") <<std::endl
+
+#define	TEST_RESULT(tr) std::cout << (tr?"TEST_PASSED":"TEST_NOT_PASSED") << std::endl; \
+		return (tr==true?0:1);
+
+
+#endif /* __TBENCH_H__ */
+
+
--- src/SHDR/shdr_src.cpp
+++ src/SHDR/shdr_src.cpp
@@ -52,22 +52,8 @@ ShaderSource::ShaderSourceState ShaderSource::state(){ return m_state; }
 
 
 #ifdef TEST_BENCH
-/* Yes this is my test scheme...  ill do it later */
-#ifndef SHDR_CPP_NOTEST
-#define PASSED TRUE
-#define NOT_PASSED NOT_TRUE
+#include <TEST/tbench.h>
 
-#define UINT_TEST_OK 1
-#define UINT_TEST_FAIL 0 
-#define BOOL_TEST_OK true
-#define BOOL_TEST_FAIL false 
-
-
-#define TEST_INT(prb,xpr) prb = prb &(xpr==1?true:false) \
-	std::cout << __FILE__ << ":" << __LINE__ << ":[TST]" << (prb ? "OK":"FAILED") <<std::endl
-
-#define	TEST_RESULT(tr) std::cout << (tr?"TEST_PASSED":"TEST_NOT_PASSED") << std::endl; \
-		return (tr==true?0:1);
 unsigned int shaderSource(const GLchar*shdrPath){
 
 	ShaderSource ss(shdrPath);
@@ -91,5 +77,4 @@ int main (int argc, char ** argv){
 
 }
 
-#endif
 #endif
