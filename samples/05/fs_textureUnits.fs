3b758a4942c730d3070b30ac6777e4c7303b165d
Julian Andres Guarin Reyes <julian.guarin@fitech.com.co>
2017-06-09 18:38:54 -0500

    Timed texture work in progress

--- samples/05/fs_textureUnits.fs
+++ samples/05/fs_textureUnits.fs
@@ -3,10 +3,10 @@ out vec4 xFragColor;
 in	vec2 TexCoord; 
 in 	vec3 ourColor;
 
-uniform sampler2D ourTexture1;
-uniform sampler2D ourTexture2;
-
+uniform sampler2D	ourTexture1;
+uniform sampler2D	ourTexture2;
+uniform float		mixFactor;
 void main()
 {
-    xFragColor = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, TexCoord), 0.4);
+    xFragColor = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, TexCoord), mixFactor);
 }
