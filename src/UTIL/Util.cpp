e0ce24a449e64688d0d5553e2b71af924e359202
Julian Andres Guarin Reyes <julian.guarin@fitech.com.co>
2017-06-13 17:51:22 -0500

    UTILS FOR INITIALIZATION

---
+++ src/UTIL/Util.cpp
@@ -0,0 +1,79 @@
+
+
+
+#include "Util.h"
+
+OSWindowWrapper::OSWindowWrapper(int iAppWidth, int iAppHeight, std::string sAppTitle):
+	m_sAppTitle(sAppTitle),
+	m_iWidth(iAppWidth),
+	m_iHeight(iAppHeight) {
+		std::cout << "[+]OSWrapper" << std::endl;
+	}
+OSWindowWrapper::~OSWindowWrapper(){
+		std::cout << "[-]OSWrapper" << std::endl; 
+ 	};
+
+ /*This ctor must be implemented for each Helper/Toolkit/Etc you wanna to implement. Only the ctor can write the value of the m_bValid flag.*/
+OSWindowWrapperGLFW::OSWindowWrapperGLFW (int iWidth, int iHeight, std::string rsAppName):
+	OSWindowWrapper(iWidth,iHeight,rsAppName){
+	std::cout << "[+]GLFW -- Wrapper" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	m_pxWindow = glfwCreateWindow(iWidth,iHeight,m_sAppTitle.c_str(), nullptr, nullptr); 
+	if (m_pxWindow){
+		std::cout << "GLFW -- Wrapper: Cool." << std::endl;
+		m_bValid = true;
+		glfwMakeContextCurrent(m_pxWindow);
+
+	} else {
+		std::cout << "GLFW -- Wrapper: Failed." << std::endl;
+		m_bValid = false;
+
+	}
+}
+OSWindowWrapperGLFW::~OSWindowWrapperGLFW () {
+	std::cout << "[-]GLFW -- Wrapper: Destroyed" << std::endl;
+	// If window then:
+	if (m_pxWindow) glfwDestroyWindow(m_pxWindow);
+	// Terminate
+	glfwTerminate();
+	std::cout << "GLFW -- Terminated" << std::endl;
+}
+GLFWwindow*OSWindowWrapperGLFW::operator()(){
+	return m_pxWindow;
+}
+bool OSWindowWrapperGLFW::bOSWWCloseEventReceived(){
+	return glfwWindowShouldClose(m_pxWindow); 
+}
+void OSWindowWrapperGLFW::vOSWWRaiseCloseEvent(){
+	glfwSetWindowShouldClose(m_pxWindow, true);
+};
+
+
+OGLManager::OGLManager(){}
+OGLManager * OGLManager::init(){
+
+	if (!s_instance){
+		s_instance = new OGLManager;
+		if (s_instance){
+			// Initialize GLEW to setup the OpenGL Function pointers
+			glewExperimental = GL_TRUE;
+			if (glewInit() != GLEW_OK)
+			{
+				std::cout << "Failed to initialize GLEW" << std::endl;
+				delete s_instance;
+			} else {
+				std::cout << "Success initialized GLEW cool (+)" << std::endl;
+				std::cout << "OGLManager::OGLInit (+)" << std::endl;
+			}
+		} else {
+			std::cout << "Failed OGL Singleton (+)" << std::endl;
+		}
+	}
+	return s_instance;
+}
+OGLManager * OGLManager::s_instance = 0;
---
+++ src/UTIL/Util.h
@@ -0,0 +1,46 @@
+#ifndef __UTIL__H__
+#define __UTIL__H__
+
+#include <iostream>
+#include <string>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class OSWindowWrapper {
+
+protected:
+	bool m_bValid;
+	std::string m_sAppTitle;
+	int m_iWidth, m_iHeight;
+public:
+	OSWindowWrapper(int iAppWidth, int iAppHeight, std::string sAppTitle);
+	virtual ~OSWindowWrapper();
+
+	virtual bool bOSWWCloseEventReceived()=0;
+	virtual void vOSWWRaiseCloseEvent()=0;
+};
+
+
+class OSWindowWrapperGLFW: public OSWindowWrapper {
+	GLFWwindow * m_pxWindow;
+public:
+	OSWindowWrapperGLFW(int iWidth, int iHeight, std::string rsAppName = "GLFW - Unnamed");
+	~OSWindowWrapperGLFW();
+	GLFWwindow * operator()();
+
+	bool bOSWWCloseEventReceived();
+	void vOSWWRaiseCloseEvent();
+};
+
+class OGLManager {
+	OGLManager();
+	static OGLManager * s_instance;
+public:
+	static OGLManager* init();
+
+};
+#endif /* __util__h__ */
+
+
+
