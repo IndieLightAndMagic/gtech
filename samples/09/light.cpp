79beea7dd5e1e299325e52aa1c22fd131dde635e
Julian Andres Guarin <julian.guarin@fitech.com.co>
2017-07-10 06:26:46 -0500

    Primitives and Engine

---
+++ samples/09/light.cpp
@@ -0,0 +1,13 @@
+
+
+
+
+int main (int argc, char ** argv){
+
+
+	unsigned int uiLightVAO;
+	glGenVertexArrays(1, &uiLightVAO);
+	glBindVertexArrat(uiLightVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+}
