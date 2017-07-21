be4d48b04328ae919f1a543df1fcf6c7f2fc1ff8
Julian Andres Guarin <julian.guarin@fitech.com.co>
2017-07-20 21:45:01 -0500

    First SDL Example functioning ok

--- samples/01/hello.cpp
+++ samples/01/hello.cpp
@@ -1,147 +1,154 @@
 
 #include <vector>
 #include <iostream>
-#include <GENG/g.h>
-#include <UTIL/Util.h> 
-/* First: headers: glew.h & glfw3.h */
-
-//GLEW: The OpenGL Extension Wrangler Library
-/*#define GLEW_STATIC*/
+#include <UTIL/Util.h>
 #include <GL/glew.h>
-
-//GLFW: A multi-platform library for OpenGL, OpenGL ES, Vulkan, window and input http://www.glfw.org/
-//#include <GLFW/glfw3.h>
 
 //WindowSize public struct 
 #define SCR_WIDTH 1200
 #define SCR_HEIGHT 675
-/*static struct {
-    bool r,g,b;
-}active = { false, false, false};*/
-static struct {
-    float r,g,b;
-}speed = { 0.01f, 0.01f, 0.01f };
-static struct {
-    float r,g,b;
-}color = { 0.0f, 0.0f, 0.0f };
 
 
-static bool dirty        = true;
-static bool killmainloop = false;
-void key_callback(int key, int repeat){
+/* So this is a demo of what should have a principal thread in a game.
+ * 
+ * Objects in the thread: A main thread should have a dynamic/static list of
+ * objects which are supposed to be processed. Just as a regular a thread but it
+ * will be called from main. This main thread will render and will get input from
+ * external devices such as keyboard, gamepad etc. 
+ * 
+ * Main thread should be the only one. So it could be a Singleton or a namespace
+ * with a bunch of functions, or a class with static methods all over the place.
+ * 
+ * A main thread also could be a class / structure with a bunch of specific
+ * callbacks. 
+ * 
+ * As this is a demo, we are going to oversimplify what we are doing. So a bunch
+ * of functions will do for now... in fact, a single static function called
+ * mainloop.
+ * 
+ * So Our MainThread class will inherit from GTh (A thread class).
 
-    if (key == SDLK_r && repeat){
-        color.r += speed.r;
-        dirty = true;
-        if (color.r > 1.0f) {
-            color.r = 1.0f;
-            speed.r *= -1;
-        } else if (color.r < 0.0f) {
-            color.r = 0.0f;
-            speed.r *= -1;
-        } 
-    }
-    if (key == SDLK_g && repeat){
-        color.g += speed.g;
-        dirty = true;
-        if (color.g > 1.0f) {
-            color.g = 1.0f;
-            speed.g *= -1;
-        } else if (color.g < 0.0f) {
-            color.g = 0.0f;
-            speed.g *= -1;
-        } 
-    }
-    if (key == SDLK_b && repeat){
-        color.b += speed.b;
-        dirty = true;
-        if (color.b > 1.0f) {
-            color.b = 1.0f;
-            speed.b *= -1;
-        } else if (color.b < 0.0f) {
-            color.b = 0.0f;
-            speed.b *= -1;
-        } 
-    }
-    if (key == SDLK_ESCAPE){
-        killmainloop = true;
-    }
-}
-using namespace GENG;
-class GObInputProcess:public GOb {
+	
 
-    /* Initialize keys to track */
-    std::vector<char> m_vKeysToTrack;
-    
-    void init(){
-        GOb::init();
-        std::vector<char>v {SDLK_r, SDLK_g, SDLK_b, 0x0};
-        m_vKeysToTrack = v;
-    }
+ */
 
-    /* This will be called by the thread */
-    void update(ui32 ui32delta){
-        /* Process here the Input */
-        SDL_Event e;
-        while (SDL_PollEvent(&e)!=0){
-            if (e.type == SDL_QUIT){
-                killmainloop = true;
-                if (e.type != SDL_KEYDOWN) continue; //You Can Push it... however this is not optimal, optimization is not needed now. 
-                
-                for (
-                    int index=0;
-                    (e.type==SDL_KEYDOWN) && m_vKeysToTrack[index];
-                    ++index
-                    ) if (e.key.keysym.scancode == m_vKeysToTrack[index]) key_callback(e.key.keysym.scancode, e.key.repeat);   
-            }
-        }
-    }
+class MainApp{
+	OSWindowWrapperSDL * m_ww;
+	struct {
+		float r,g,b;
+	}speed;
+	struct {
+		float r,g,b;
+	}color;
+
+
+public:
+	bool dirty        = true;
+	bool killmainloop = false;
+	
+	MainApp():
+		m_ww(0x0),
+		speed({ 0.01f, 0.01f, 0.01f }),
+		color({ 0.00f, 0.00f, 0.00f }),
+		dirty(true),
+		killmainloop(false){}
+	
+
+	void ProcessInput(){
+		static bool ok = false;
+		/* Process here the Input */
+		SDL_Event e;
+		if (SDL_PollEvent(&e) != 0){
+			
+			if (ok == false) {
+				std::cout<< "OK" << std::endl;
+			}
+			switch(e.type)
+			{
+				case SDL_KEYDOWN:
+					ProcessKey(e);
+					break;
+				case SDL_QUIT:
+					killmainloop = true;
+					break;
+			}
+		}
+	}
+
+	void ProcessKey(SDL_Event&e){
+
+		if (e.key.keysym.scancode == SDL_SCANCODE_R && e.key.repeat){
+			color.r += speed.r;
+			dirty = true;
+			if (color.r > 1.0f) {
+				color.r = 1.0f;
+				speed.r *= -1;
+			} else if (color.r < 0.0f) {
+				color.r = 0.0f;
+				speed.r *= -1;
+			} 
+		}
+		if (e.key.keysym.scancode == SDL_SCANCODE_G && e.key.repeat){
+			color.g += speed.g;
+			dirty = true;
+			if (color.g > 1.0f) {
+				color.g = 1.0f;
+				speed.g *= -1;
+			} else if (color.g < 0.0f) {
+				color.g = 0.0f;
+				speed.g *= -1;
+			} 
+		}
+		if (e.key.keysym.scancode == SDL_SCANCODE_B && e.key.repeat){
+			color.b += speed.b;
+			dirty = true;
+			if (color.b > 1.0f) {
+				color.b = 1.0f;
+				speed.b *= -1;
+			} else if (color.b < 0.0f) {
+				color.b = 0.0f;
+				speed.b *= -1;
+			} 
+		}
+		if (e.key.keysym.scancode == SDL_SCANCODE_ESCAPE){
+			killmainloop = true;
+		}
+	}
+	void Render(){
+		if (dirty){
+			glClearColor(color.r,color.g,color.b,1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			dirty = false;
+			m_ww->vOSWWSwap();
+		}
+
+	}
+	int MainLoop(){
+
+		std::unique_ptr<OSWindowWrapperSDL> _SDL_(new OSWindowWrapperSDL(SCR_WIDTH, SCR_HEIGHT));
+		m_ww = _SDL_.get();
+		std::cout << "Starting SDL context, OpenGL 3.3" << std::endl;
+		std::cout << "Press R / G / B keys!!" << std::endl;
+		
+		// Game loop: Here's he window 
+		for (;true;)
+		{
+			// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+			// Process input.
+			ProcessInput();
+			Render();
+			// Swap the screen buffers
+			if (killmainloop) break;
+		}
+		return 0;
+	}
+
 };
-
 int main ()
 {
 
-
-	std::unique_ptr<OSWindowWrapperSDL> _SDL_(new OSWindowWrapperSDL(SCR_WIDTH, SCR_HEIGHT));
-
- 	std::cout << "Starting SDL context, OpenGL 3.3" << std::endl;
-    std::cout << "Press R / G / B keys!!" << std::endl;
-
-    // Create A Thread for Objects that process Input (TI).
-    GTh xInputThread(GNULL);
-
-
-    // Create An Object which processes the input events (OI).
-    GObInputProcess xInputProcess;
-
-    // Add OI as a worker into TI.
-    xInputThread.pushObject(&xInputProcess);    
-
-
-    // Game loop: Here's he window 
-    for (;true;_SDL_->vOSWWSwap())
-    {
-        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-        // Process input.
-
-        //When some change has been made 
-        if (dirty){
-
-            glClearColor(color.r,color.g,color.b,1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-            dirty = false;
-        
-        }
-
-        // Swap the screen buffers
-        if (killmainloop) break;
-    }
-
-
-
-    return 0;
-
-    /* _SDL_ will peacefully terminate */
+	MainApp ma;
+	return ma.MainLoop();
 
 }
 
--- src/GENG/g.h
+++ src/GENG/g.h
@@ -20,7 +20,7 @@ typedef Uint32 ui32;
 
 
 class GOb {
-
+	int id;
 	ui32 m_ticks;
 	bool m_started;
 public:
@@ -29,21 +29,16 @@ public:
 	void _update();
 	virtual void update(ui32 ui32delta);
 
-
+	GOb();
+	~GOb();
 	
 
-	GOb(){
-		cout <<"+GOb"<<endl;
-		init();
-	}
-	~GOb(){
-		cout <<"-GOb"<<endl;
-	}
 
 };
 
 
 class GTh{
+	int id;
 	SDL_Thread * m_pxT;
 	vector<GOb*> m_vO;
 
@@ -55,7 +50,17 @@ public:
 	void killThread(){
 		bTAlive = false;
 	}
+	/**
+	 * @brief      Determines if alive.
+	 *
+	 * @return     True if alive, False otherwise.
+	 */
 	bool isAlive(){ return bTAlive; }
+	/**
+	 * @brief      This function runs the thread. As long the thread is alive it must be called. The task of run is to update all the objects in m_vO.
+	 *
+	 * @return     The function should return 0. Otherwise will return another value.
+	 */
 	virtual int run();
 	GTh(void * param);
 	~GTh();
