bba662be7769448c193e6ca2d22fee8baefaef16
Julian Guarin <julian.guarin@fitech.com.co>
2017-11-24 08:25:58 -0500

    Templated properties

--- src/MESHCOMPONENT/GMaterialComponent.h
+++ src/MESHCOMPONENT/GMaterialComponent.h
@@ -23,7 +23,7 @@
 
 using Ptr = const unsigned char*;
 using Byte = unsigned char;
-class GPropertyValue
+class IGPropertyValue
 {
 protected:
 	std::size_t m_sz;
@@ -36,7 +36,7 @@ protected:
 	 * @param[in]  sz    The size
 	 * @param[in]  ptr   The pointer
 	 */
-	GPropertyValue(std::size_t sz, Ptr ptr = nullptr):
+	IGPropertyValue(std::size_t sz, Ptr ptr = nullptr):
 	m_sz(sz)
 	{
 		m_uptr = std::make_unique<Byte>(sz);
@@ -61,13 +61,38 @@ public:
 		//Copy if memory its being passed.
 		std::memcpy((void*)m_ptr, propertyValueAddress, m_sz);
 	}
-	virtual ~GPropertyValue(){
+	virtual ~IGPropertyValue(){
 		std::cout << "Goodbye! @" << std::hex << this << "\n"; 
 	}
 	
 
 };
+template <typename T>
+class GPropertyValue : public IGPropertyValue
+{
+public:
+	GPropertyValue(std::size_t sz = sizeof(T), T *tptr = nullptr):
+	IGPropertyValue(sz,reinterpret_cast<Ptr>(tptr))
+	{
+		std::cout << "Derived::\n";
+	}
+	~GPropertyValue(){std::cout << "Derived::\n";};
 
+	void setPropertyValue(const T *propertyValueAddress)
+	{
+		Ptr ptr = reinterpret_cast<Ptr>(propertyValueAddress);
+		IGPropertyValue::setPropertyValue(ptr,sizeof(T));
+	}
+	void setPropertyValue(const T &propertyValue)
+	{
+		setPropertyValue(&propertyValue);
+	}
+	T getPropertyValue()
+	{
+		T *tptr = reinterpret_cast<T*>(getPtr());
+		return *tptr;
+	}
+};
 
 
 #endif /*__GMATERIALCOMPONENT_H__*/
--- src/MESHCOMPONENT/tbench/prop/prop.cpp
+++ src/MESHCOMPONENT/tbench/prop/prop.cpp
@@ -4,35 +4,11 @@
 
 using namespace std;
 
-class GPropertyValueInt : public GPropertyValue
-{
-public:
-	GPropertyValueInt(std::size_t sz = sizeof(int), int * iptr = nullptr):
-	GPropertyValue(sz,reinterpret_cast<Ptr>(iptr))
-	{
-		std::cout << "Derived::\n";
-	}
-	~GPropertyValueInt(){std::cout << "Derived::\n";};
 
-	void setPropertyValue(const int *propertyValueAddress)
-	{
-		Ptr ptr = reinterpret_cast<Ptr>(propertyValueAddress);
-		GPropertyValue::setPropertyValue(ptr,sizeof(int));
-	}
-	void setPropertyValue(const int &propertyValue)
-	{
-		setPropertyValue(&propertyValue);
-	}
-	int getPropertyValue()
-	{
-		int *iptr = reinterpret_cast<int*>(getPtr());
-		return iptr[0];
-	}
-};
 int main(void)
 {
 	//A 10 Bytes in Size Property.
-	std::unique_ptr<GPropertyValue> pprop = std::make_unique<GPropertyValueInt>();
+	std::unique_ptr<IGPropertyValue> pprop = std::make_unique<GPropertyValue<int>>();
 
 	cout << "Test program for GMaterialComponent\n";
 	return 0;
